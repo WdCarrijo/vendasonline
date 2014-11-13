2afc7e5c2c7a8a5214792cf0c7926eac8d0862f8
rof20004 <rof20004@gmail.com>
2014-11-13 16:27:34 -0400

    Commit das exemptions.

---
+++ src/main/java/br/com/vendasonline/exception/ClienteJaExistenteException.java
@@ -0,0 +1,7 @@
+package br.com.vendasonline.exception;
+
+public class ClienteJaExistenteException extends IllegalArgumentException {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/br/com/vendasonline/exception/ErroGenericoException.java
@@ -0,0 +1,7 @@
+package br.com.vendasonline.exception;
+
+public class ErroGenericoException extends IllegalArgumentException {
+
+	private static final long serialVersionUID = 1L;
+
+}
